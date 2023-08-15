e71d4d5eb4eb4d7f7c89f12aa73ec7ba0049ee00
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2023-08-16 00:51:05 +0900

    重複・負の値に対応

--- code/ch12_3.R
+++ code/ch12_3.R
@@ -18,28 +18,34 @@ insertion_sort <- function(vec) {
   N <- length(vec)
   
   # 要素ごとに処理
-  for(i in 2:N) { # (初回は不要)
+  for(i in 2:N) { # (1番目は不要)
     
     # i番目の値を取得
     val <- vec[i]
     
-    # 挿入位置jの探索:(j < i)
-    for(j in i:1) { # (逆順に処理)
-      if(j > 1) { # (1番目は不要)
+    # i番目から逆順に挿入位置jを探索:(j ≤ i)
+    for(j in (i-1):0) { # (i番目は不要・0番目は挿入処理との兼ね合い用)
+      
+      # j番目とi番目の値を大小比較
+      if(j <= 0) {
         
-        if(vec[j-1] > val) {
-          # j番目の値を入替
-          vec[j] <- vec[j-1]
-        } else {
-          # 挿入位置が定まれば終了
-          break
-        }
+        # 全要素を比較したらループを終了
+        break
         
+      } else if(vec[j] > val) {
+        
+        # j番目の値を1つ後に移動
+        vec[j+1] <- vec[j]
+        
+      } else {
+        
+        # i番目の値以下ならループを終了
+        break
       }
     }
     
     # i番目の値をj番目に挿入
-    vec[j] <- val
+    vec[j+1] <- val
   }
   
   # 数列を出力
@@ -48,36 +54,39 @@ insertion_sort <- function(vec) {
 
 
 # 要素数を指定
-N <- 10
+N <- 25
 
 # 数列を生成
-a <- sample(x = 1:N, size = N, replace = FALSE)
-a
+a <- sample(x = 1:(2*N), size = N, replace = TRUE)
+a <- rnorm(n = N, mean = 0, sd = 1) |> 
+  round(digits = 1)
+a; table(a)
 
 # ソート
 insertion_sort(a)
+sum(!(insertion_sort(a) == sort(a)))
 
 
 # 可視化 ---------------------------------------------------------------------
 
 # 数列を指定
-a <- c(4, 1, 3, 5, 2)
+#a <- c(4, 1, 3, 5, 2)
 
 # 要素数を取得
 N <- length(a)
 
 # 数列を格納
 tmp_df <- tibble::tibble(
-  i   = 0,   # 試行回数
-  idx = 1:N, # インデックス
-  a   = a    # 要素
+  iteration = 0,   # 試行回数
+  id        = 1:N, # 元のインデックス
+  index     = 1:N, # 各試行のインデックス
+  value     = a    # 要素
 )
 
 # 数列を作図
 ggplot() + 
   geom_bar(data = tmp_df, 
-           mapping = aes(x = idx, y = a, fill = factor(a)), 
-           stat = "identity") + 
+           mapping = aes(x = index, y = value, fill = factor(value)), stat = "identity") + 
   theme(panel.grid.minor.x = element_blank()) + 
   labs(title = "insertion sort", 
        subtitle = paste0("i = ", unique(tmp_df[["i"]])), 
@@ -85,103 +94,139 @@ ggplot() +
        x = "index", y = "value")
 
 
-# 数列を記録
+# 作図用のオブジェクトを初期化
+id_vec   <- 1:N
 trace_df <- tmp_df
 
 # 挿入ソート
 for(i in 1:N) {
-  
-  # i番目の値を取得
-  v <- a[i]
-  
-  # 挿入位置jの探索:(j < i)
-  for(j in i:1) {
-    if(j > 1) {
+  if(i > 1) { # (初回は不要)
+    
+    # i番目の値を取得
+    v <- a[i]
+    
+    # i番目から逆順に挿入位置jを探索:(j ≤ i)
+    for(j in (i-1):0) { # (i番目は不要・0番目は挿入処理との兼ね合い用)
       
-      if(a[j-1] > v) {
-        # j番目の値を入替
-        a[j] <- a[j-1]
+      # j番目とi番目の値を大小比較
+      if(j <= 0) {
+        
+        # 全要素を比較したらループを終了
+        break
+        
+      } else if(a[j] > v) {
+        
+        # j番目の値を1つ後に移動
+        a[j+1] <- a[j]
+        id_vec[1:N] <- replace(x = id_vec, list = j+1, values = id_vec[j])
+        
       } else {
-        # 挿入位置が定まれば終了
+        
+        # i番目の値以下ならループを終了
         break
       }
-      
     }
+    
+    # i番目の値をj番目に挿入
+    a[j+1] <- v
+    id_vec[1:N] <- replace(x = id_vec, list = j+1, values = i)
   }
-  
-  # i番目の値をj番目に挿入
-  a[j] <- v
   
   # 数列を格納
   tmp_df <- tibble::tibble(
-    i   = i, 
-    idx = 1:N, 
-    a   = a
+    iteration = i, 
+    id        = id_vec, 
+    index     = 1:N, 
+    value     = a
   )
   
   # 数列を記録
   trace_df <- dplyr::bind_rows(trace_df, tmp_df)
   
   # 途中経過を表示
-  print(paste0("--- i:", i, " ---"))
+  print(paste0("--- iteration:", i, " ---"))
   print(a)
 }
 
 
-# 挿入データを抽出
+# 挿入データを作成
 target_df <- trace_df |> 
-  dplyr::filter(i == idx) |> 
-  dplyr::mutate(
-    i = i - 1, 
-    y = max(a)
-  )
+  dplyr::filter(iteration == id) # i番目の要素を抽出
 
-# 全ての試行の数列を作図
+# 全試行の数列を作図
 ggplot() + 
   geom_bar(data = trace_df, 
-           mapping = aes(x = idx, y = a, fill = factor(a)), stat = "identity") + 
+           mapping = aes(x = index, y = value, fill = factor(value)), stat = "identity") + # 全ての要素
   geom_bar(data = target_df,
-           mapping = aes(x = idx, y = y, group = factor(a)), stat = "identity",
-           color = "red", alpha = 0, linewidth = 1, linetype = "dashed") + # i番目の要素
-  facet_wrap(i ~ ., scales = "free_x", labeller = label_both) + # 試行ごとに分割
+           mapping = aes(x = id, y = value), stat = "identity",
+           color = "red", alpha = 0, linewidth = 0.6, linetype = "dashed") + # 挿入前のi番目の要素
+  geom_bar(data = insert_df,
+           mapping = aes(x = index, y = value), stat = "identity",
+           color = "red", alpha = 0, linewidth = 0.6, linetype = "dotted") + # 挿入後のi番目の要素
+  geom_segment(data = target_df, 
+               mapping = aes(x = id, y = value, xend = -Inf, yend = value, color = factor(value)), 
+               linewidth = 0.6, linetype = "dashed", show.legend = FALSE) + # 挿入データまでの上限値
+  facet_wrap(iteration ~ ., scales = "free_x", labeller = label_both) + # 試行ごとに分割
   theme(panel.grid.minor.x = element_blank()) + 
   labs(title = "insertion sort", 
        fill = "value", 
        x = "index", y = "value")
 
 
-# 挿入データを抽出
+# 挿入データを作成
 target_df <- trace_df |> 
-    dplyr::filter((i+1) == idx)
+    dplyr::filter((iteration + 1) == index) # i番目の要素を抽出
+
+# 重複ラベルを作成
+dup_label_df <- trace_df |> 
+  dplyr::arrange(iteration, id) |> # IDの割当用
+  dplyr::group_by(iteration, value) |> # IDの割当用
+  dplyr::mutate(
+    dup_id    = dplyr::row_number(id), # 重複IDを割り当て
+    dup_num   = max(dup_id), # 重複の判定用
+    dup_label = dplyr::if_else(
+      condition = dup_num > 1, 
+      true = paste0("(", dup_id, ")"), 
+      false = ""
+    ) # 重複要素のみラベルを作成
+  ) |> 
+  dplyr::ungroup() |> 
+  dplyr::arrange(iteration, index)
 
 # ソートのアニメーションを作図
 graph <- ggplot() + 
   geom_bar(data = trace_df, 
-           mapping = aes(x = idx, y = a, fill = factor(a)), stat = "identity") + # 全ての要素
+           mapping = aes(x = index, y = value, fill = factor(value), group = factor(id)), stat = "identity") + # 全要素
   geom_bar(data = target_df,
-           mapping = aes(x = idx, y = a, group = factor(a)), stat = "identity",
-           color = "red", alpha = 0, linewidth = 1, linetype = "dashed") + # i番目の要素
+           mapping = aes(x = index, y = value, group = factor(id)), stat = "identity",
+           color = "red", alpha = 0, linewidth = 1, linetype = "dashed") + # 挿入データ
+  geom_segment(data = target_df,
+               mapping = aes(x = index, y = value, xend = -2, yend = value,
+                             color = factor(value), group = factor(id)),
+               linewidth = 1, linetype = "dashed", show.legend = FALSE) + # 挿入データまでの上限値
   geom_text(data = trace_df, 
-            mapping = aes(x = idx, y = 0, label = as.character(a), group = factor(a)), 
+            mapping = aes(x = index, y = 0, label = as.character(value), group = factor(id)), 
             vjust = -0.5, size = 5) + # 要素ラベル
-  gganimate::transition_states(i, transition_length = 9, state_length = 1, wrap = FALSE) + # フレーム遷移
+  geom_text(data = dup_label_df, 
+            mapping = aes(x = index, y = 0, label = dup_label, group = factor(id)), 
+            vjust = 1, size = 4) + # 重複ラベル
+  gganimate::transition_states(states = iteration, transition_length = 9, state_length = 1, wrap = FALSE) + # フレーム遷移
   gganimate::ease_aes("cubic-in-out") + # 遷移の緩急
-  theme(panel.grid.minor.x = element_blank()) + 
+  coord_cartesian(xlim = c(0, N+1)) + # (上限値の線用)
+  theme(panel.grid.minor.x = element_blank(), 
+        legend.position = "none") + 
   labs(title = "insertion sort", 
-       subtitle = "i = {next_state}", 
+       subtitle = "iteration : {next_state}", 
        fill = "value", 
        x = "index", y = "value")
 
-# 一時停止フレーム数を指定
-p <- 10
-
 # 1試行当たりのフレーム数を指定
-s <- 10
+s <- 20
 
 # gif画像を作成
 gganimate::animate(
   plot = graph, 
-  nframes = (N+1 + 2*p)*s, start_pause = p, end_pause = p, fps = 50, 
+  nframes = (N+1 + 2)*s, start_pause = s, end_pause = s, fps = 20, 
   width = 800, height = 600, 
   renderer = gganimate::gifski_renderer()
 )
